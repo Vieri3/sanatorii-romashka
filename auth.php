29ca7e8eb2260c2a368d071cd2ddd9f1d10f3f14
Feliks N <fnow01@gmail.com>
2024-09-20 16:28:17 +0200

    Использовать шаблон формы на страницах входа и регистрации

--- auth.php
+++ auth.php
@@ -1,31 +1,7 @@
 <?php include "header.php" ?>
 
 <div class="container">
-	<div id="form-msg" class="alert alert-warning d-none"></div>
-	<div class="row">
-		<div class="col-3"></div>
-		<div class="col-6">
-			<div class="card">
-				<div class="card-body">
-					<h3 class="card-title">Авторизация</h3>
-					<form id="form-auth" method="POST" action="/admin/auth.php">
-						<div class="form-group">
-							<label for="name">Имя</label>
-							<input type="text" name="name" class="form-control" placeholder="Имя">
-						</div>
-						<br>
-						<div class="form-group">
-							<label for="password">Пароль</label>
-							<input type="password" name="password" class="form-control" placeholder="Пароль">
-						</div>
-						<br>
-						<button type="submit" class="btn btn-success">Авторизоваться</button>
-					</form>
-				</div>
-			</div>
-		</div>
-		<div class="col-3"></div>
-	</div>
+	<?php include 'parts/form-access.php'; ?>
 </div>
 
 <?php include "footer.php" ?>
