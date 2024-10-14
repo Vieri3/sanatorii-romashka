5a4a0ad4161fc48f11ae91bc1c4b5a44fe682c55
Feliks N <feliks.nvk@gmail.com>
2024-10-14 17:40:06 +0200

    Исправить стили магазина с добавлением навигации

--- shop.php
+++ shop.php
@@ -1,32 +1,33 @@
 <?php include "header.php"?>
 
+<div class="container-fluid bg-primary-subtle py-5">
+    <div class="d-flex justify-content-center">
 
-<div class="wrap-shop">
-		<div class="card-shop">
-			<div class="front-shop front-1-shop"></div>
-			<div class="back-shop">
-				<p>Травы, отвары и пр</p>
-				<a href="#">ссылка на товары</a>
-			</div>
-		</div>
-
-		<div class="card-shop">
-			<div class="front-shop front-2-shop"></div>
-			<div class="back-shop">
-				<p>эко вещи и фирменные вещи от санатория</p>
-				<a href="#">ссылка на товары</a>
-			</div>
-		</div>
-
-		<div class="card-shop">
-			<div class="front-shop front-3-shop"></div>
-			<div class="back-shop">
-				<p>Сувениры, и прочее барахло</p>
-				<a href="#">ссылка на товары</a>
-			</div>
-		</div>
-	</div>
-
-
+        <div class="card-shop">
+            <div class="front-shop front-1-shop"></div>
+            <div class="back-shop">
+                <p>Травы, отвары и пр</p>
+                <a href="#">ссылка на товары</a>
+            </div>
+        </div>
+    
+        <div class="card-shop">
+            <div class="front-shop front-2-shop"></div>
+            <div class="back-shop">
+                <p>эко вещи и фирменные вещи от санатория</p>
+                <a href="#">ссылка на товары</a>
+            </div>
+        </div>
+    
+        <div class="card-shop">
+            <div class="front-shop front-3-shop"></div>
+            <div class="back-shop">
+                <p>Сувениры, и прочее барахло</p>
+                <a href="#">ссылка на товары</a>
+            </div>
+        </div>
+        
+    </div>
+</div>
 
 <?php include "footer.php" ?>
