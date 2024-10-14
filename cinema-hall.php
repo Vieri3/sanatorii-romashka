800451db7a91d90a3a332610df9d8555efb9157e
Feliks N <feliks.nvk@gmail.com>
2024-10-14 10:31:51 +0200

    Не использовать доп. параметр без необходимости

--- cinema-hall.php
+++ cinema-hall.php
@@ -1,7 +1,7 @@
 <?php 
     include "header.php";
     $movie_id = $_GET['id'];
-    $movie = selectDBdata("SELECT * FROM `cinema` WHERE `cinema`.`cinema_id` = '$movie_id'")[0];
+    $movie = selectDBdata("SELECT * FROM `cinema` WHERE `cinema_id` = '$movie_id'")[0];
     $movie_seats = explode(",", $movie['cinema_place']);
 ?>
 
